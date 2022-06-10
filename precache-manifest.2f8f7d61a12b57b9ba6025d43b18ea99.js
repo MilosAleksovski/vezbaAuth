d8e18029c7c00d0e4cbe2fb7bbd32a4c5334251f
Milos Aleksovski <maleksovski10920rn@raf.rs>
2022-06-10 13:23:55 +0200

    Updates

---
+++ precache-manifest.2f8f7d61a12b57b9ba6025d43b18ea99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dceb7f2f43c8a7820afa5ece422b5bd",
+    "url": "/vezbaAuth/index.html"
+  },
+  {
+    "revision": "c984fbf55159d7bed538",
+    "url": "/vezbaAuth/static/css/main.f072c70a.chunk.css"
+  },
+  {
+    "revision": "42330ae85b2e812516c8",
+    "url": "/vezbaAuth/static/js/2.e48ddb93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vezbaAuth/static/js/2.e48ddb93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c984fbf55159d7bed538",
+    "url": "/vezbaAuth/static/js/main.fb358b5b.chunk.js"
+  },
+  {
+    "revision": "bcadcf1f6ed829926c09",
+    "url": "/vezbaAuth/static/js/runtime-main.ad1c07d1.js"
+  }
+]);
